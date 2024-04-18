9de1fc13b43855287a1e7e96d352eecf484631ff
Ares201 <jcerveracobenas@gmail.com>
2024-04-17 23:15:12 -0500

    cambios

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from './pages/CoffeList.vue'
+import App from './pages/starkNetWork.vue'
 import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
---
+++ src/pages/starkNetWork.vue
@@ -0,0 +1,19 @@
+<template>
+   <div>
+     <h1>xd</h1>
+   </div>
+</template>
+<script>
+export default {
+   data() {
+     return {
+       
+     }
+   },
+   methods: {
+     
+   }
+};
+</script>
+<style lang='' scoped>
+</style>
